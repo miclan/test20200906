fca5eacd2c70f8ab271dcf2700b93e3da8896cf8
miclan@163.com <lan@landeMacBook-Pro.local>
2020-11-22 22:31:28 +0800

    remove unused imports

--- src/main/java/com/miclan/x/test20200906/controller/HeavyController.java
+++ src/main/java/com/miclan/x/test20200906/controller/HeavyController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import avro.shaded.com.google.common.collect.Lists;
-
 @RestController
 @RequestMapping("/heavy")
 public class HeavyController {
